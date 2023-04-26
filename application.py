f5e379f5fa0381c9710281c032fb01abe0e853c7
Enrique Munoz <enriquemunozdev@gmail.com>
2023-04-26 00:34:40 -0600

    Started making some stuff

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from dotenv import load_dotenv
 
@@ -13,4 +13,4 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
